5c61be381ace0e0b1e3dc7132bd3a373da1a7fd6
Gary Mawdsley <gmawdsley@inviqa.com>
2019-05-11 22:43:32 +0100

    Test deploy

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 export default () => (
   <div>
-    Hello World.{' '}
+    Hello World.{' '}!
     <Link href='/about' as={process.env.BACKEND_URL + '/about'}>
       <a>About</a>
     </Link>
